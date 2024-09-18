46714c1e52f436710f1d61e897626557b72f33bc
Claudio Barbera <barbera.claudio@gmail.com>
2024-09-18 10:46:53 +0200

    Make public

--- Source/model/scene/Node.swift
+++ Source/model/scene/Node.swift
@@ -3,7 +3,8 @@ import Foundation
 open class Node: Drawable {
 
     public let placeVar: AnimatableVariable<Transform>
-    var dataAttributes: [(key: String, value: String)]?
+    public var dataAttributes: [(key: String, value: String)]?
+    
     open var place: Transform {
         get { return placeVar.value }
         set(val) { placeVar.value = val }
